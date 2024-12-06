219c5060c17e6f1b880e1d132de41506cf1afeeb
nandhiesswaran <161180477+nandhiesswaran@users.noreply.github.com>
2024-12-06 08:19:08 +0530

    Update four.c

--- four.c
+++ four.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
+    int main() {
+    int a = 5, b = 10, c, d;
+    
+    printf("Generating TAC for the expression: d = a + b * 2\n");
+ 
+    printf("t1 = b * 2\n"); // t1 = b * 2
+    printf("t2 = a + t1\n"); // t2 = a + t1
+    printf("d = t2\n");
+    
+    c = b * 2; // t1 = b * 2
+    d = a + c; // t2 = a + t1, d = t2
 
-int four() {
-    int a = 5, b = 2, c, d;
-    printf("Tac for d = a + b * 2\n");
-
-    c = b*2;
-    d = a + c;
-
-    printf("%d", d);
+    printf("Result of the expression d = a + b * 2 is: d = %d\n", d);
     return 0;
 }
